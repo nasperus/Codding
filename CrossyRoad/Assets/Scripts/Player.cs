0eedf7d04c9ef299688c4482aef10206d013115a
Giorgi <gio.melikidze095@gmail.com>
2021-06-13 13:44:03 +0400

    code update

--- CrossyRoad/Assets/Scripts/Player.cs
+++ CrossyRoad/Assets/Scripts/Player.cs
@@ -6,8 +6,7 @@ public class Player : MonoBehaviour
 {
     private Rigidbody player;
     [SerializeField] private  float speed;
-    private bool dMovement = true;
-    private bool aMovement = true;
+
 
 
     void Start()
@@ -21,64 +20,31 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
+            transform.rotation = Quaternion.LookRotation(Vector3.back);
             transform.Translate(Vector3.back * speed * Time.deltaTime);
+            
         }
-
-
-    
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            AMovement();
-        }
-      
-
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            DMovement();
-        }
-
-     
-        
-    }
-
-
-    private void DMovement()
-    {
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            if (dMovement)
-            {
-                transform.Rotate(0, -270, 0);
-            }
-
-            dMovement = false;
-          
+            transform.rotation = Quaternion.LookRotation(Vector3.left);
             transform.Translate(Vector3.back * speed * Time.deltaTime);
 
-
-
         }
-    }
 
-    private void AMovement()
-    {
         if (Input.GetKeyDown(KeyCode.A))
         {
-
-            if (aMovement)
-            {
-                transform.Rotate(0, 270, 0);
-            }
-
-            aMovement = false;
-
+            transform.rotation = Quaternion.LookRotation(Vector3.right);
             transform.Translate(Vector3.back * speed * Time.deltaTime);
 
-            
 
 
         }
+
     }
+
+
+  
+  
 
 }
